290ca89e51cfe6e10edafb95bd69a8a28553cb84
Jose Eugenio Duran <eugenioduran15@gmail.com>
2019-01-15 16:26:13 -0400

    Changing Util file

--- app/src/main/java/com/android/diplomado/guessingapp/utils/Util.java
+++ app/src/main/java/com/android/diplomado/guessingapp/utils/Util.java
@@ -10,8 +10,8 @@ public class Util {
     public static List<Category> getDummyData(){
         return new ArrayList<Category>(){{
             add(new Category("Peliculas", "Adivina las peliculas", "https://as01.epimg.net/meristation/imagenes/2018/01/22/reportaje/1516604400_722996_1531823096_sumario_normal.jpg"));
-            add(new Category("Cantantes", "Adivina los cantantes", "http://info7rm.blob.core.windows.net.optimalcdn.com/images/2017/11/26/musica.jpg"));
-            add(new Category("Libros", "Adivina los libros", "http://www.raicesdeeuropa.com/wp-content/uploads/li.jpg"));
+            add(new Category("Star Wars", "Adivina los personaje de star wars", "https://img.europapress.es/fotoweb/fotonoticia_20180402130008_640.jpg"));
+            add(new Category("Tv Shows", "Adivina las series", "https://studybreaks.com/wp-content/uploads/2018/02/4-1.jpg"));
             add(new Category("Paises", "Adivina los paises", "https://cdn.noticiaaldia.com/wp-content/uploads/2016/08/banderas.jpg"));
             add(new Category("Animes", "Adivina los animes", "https://i0.wp.com/culturageek.com.ar/wp-content/uploads/2017/03/Naruto_primera_parte_HD.png"));
         }};
